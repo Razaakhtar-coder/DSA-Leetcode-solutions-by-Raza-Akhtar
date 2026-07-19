a8b2fe1d5b7d9993fe7fdaedda59e97c52279eab
Razaakhtar-coder <akhtarrakeeb52@gmail.com>
2026-07-19 21:39:42 +0530

    Time: 0 ms (100%), Space: 12.6 MB (26.88%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int max = *max_element(candies.begin(), candies.end());
+
+        vector<bool> ans;
+
+        for(auto candy: candies){
+            ans.push_back(candy + extraCandies >= max); 
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
