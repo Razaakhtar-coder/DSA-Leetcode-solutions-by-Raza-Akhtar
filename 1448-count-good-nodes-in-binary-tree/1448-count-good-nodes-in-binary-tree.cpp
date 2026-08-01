c106533787717424173770a2783dd15750336621
Razaakhtar-coder <akhtarrakeeb52@gmail.com>
2026-08-01 18:58:35 +0530

    Time: 91 ms (82.73%), Space: 88.2 MB (85.08%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* root, int maxSoFar) {
+       if(root == NULL) return 0;
+       int count = 0;
+
+       if(root->val >= maxSoFar){
+         count = 1;
+       }
+       maxSoFar = max(maxSoFar, root->val);
+
+       count += dfs(root->left, maxSoFar);
+       count += dfs(root->right, maxSoFar);
+
+       return count;
+    }
+
+    int goodNodes(TreeNode* root){
+        return dfs(root, root->val);
+    }
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
