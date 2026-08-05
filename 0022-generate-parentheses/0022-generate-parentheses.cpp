eda9317cdfc4703dbaf09475193b94fc98a8ebdd
Razaakhtar-coder <akhtarrakeeb52@gmail.com>
2026-08-05 20:31:07 +0530

    Time: 3 ms (69.11%), Space: 16.1 MB (26.41%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    void backtrack(string s, int open, int close, int n) {
+        if (s.size() == 2 * n) {
+            ans.push_back(s);
+            return;
+        }
+
+        if (open < n) {
+            backtrack(s + "(", open + 1, close, n);
+        }
+
+        if (close < open) {
+            backtrack(s + ")", open, close + 1, n);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        backtrack("", 0, 0, n);
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
