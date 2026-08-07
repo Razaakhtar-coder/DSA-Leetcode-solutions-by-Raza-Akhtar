2701039c5277d8d953c82ac1570a5ee200121314
Razaakhtar-coder <akhtarrakeeb52@gmail.com>
2026-08-07 15:57:29 +0530

    Time: 0 ms (100%), Space: 12.9 MB (70.33%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int n;
+    int dp[201][201];
+    bool vis[201][201];
+
+    int solve(vector<vector<int>>& triangle, int row, int col) {
+        if (row == n - 1)
+            return triangle[row][col];
+
+        if (vis[row][col])
+            return dp[row][col];
+
+        vis[row][col] = true;
+
+        int minPath = triangle[row][col] +
+                      min(solve(triangle, row + 1, col),
+                          solve(triangle, row + 1, col + 1));
+
+        return dp[row][col] = minPath;
+    }
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        n = triangle.size();
+        memset(vis, 0, sizeof(vis));
+        return solve(triangle, 0, 0);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
