42281edcf30265738170f7ea78fc10d50c1aa722
Razaakhtar-coder <akhtarrakeeb52@gmail.com>
2026-08-07 16:05:22 +0530

    Time: 0 ms (100%), Space: 12.7 MB (84.48%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -27,6 +27,27 @@ public:
     }
 };
 
+// bottom up code below
+
+//T.C : O(n^2)
+//S.C : O(n^2)
+// class Solution {
+// public:
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int n = triangle.size();
+//         vector<vector<int>> t = triangle;  // copy triangle for t
+
+//         // Start from second last row and move upwards
+//         for (int row = n - 2; row >= 0; row--) {
+//             for (int col = 0; col <= row; col++) { //1st row me 1 column, 2nd Row me 2 Columns, and so on, that's why (col <= row)
+//                 t[row][col] += min(t[row + 1][col], t[row + 1][col + 1]);
+//             }
+//         }
+
+//         return t[0][0];  // minimum path sum
+//     }
+// };
+
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
 // Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
