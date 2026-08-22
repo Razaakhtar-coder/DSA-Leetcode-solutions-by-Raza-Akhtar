e66ced4b6b67fecc65154fa02c4e8eecce71bb4c
Razaakhtar-coder <akhtarrakeeb52@gmail.com>
2026-08-22 15:06:46 +0530

    Time: 0 ms (100%), Space: 7.7 MB (97.17%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool checkDivisibility(int n) {
-        int ans = n;
+        int ans = n; // why storing n because later we r going to change n while extracting its digit
         int sum = 0;
-        int product = 1;
+        int product = 1; //We start with 1 because 1 is the multiplicative identity
 
         while(n != 0){
-            int lastDigit = n % 10;
+            int lastDigit = n % 10; // gives the last digit of number
             sum += lastDigit;
             product *= lastDigit;
-            n = n / 10;
+            n = n / 10; // remove the last digit
         }
         int finalOutcome = sum + product;
-        return (ans % finalOutcome) == 0;
+        return (ans % finalOutcome) == 0; //checks whether ans is perfectly divisible by finalOutcome.
     }
 };
 
