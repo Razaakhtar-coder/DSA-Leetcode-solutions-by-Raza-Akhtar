25d0ebfd8b6223cc55f6b4ca8c0006240bb787d9
Razaakhtar-coder <akhtarrakeeb52@gmail.com>
2026-08-25 01:53:38 +0530

    Time: 0 ms (100%), Space: 22.4 MB (73.89%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     pair<int, int> solve(TreeNode* root) {
-        if (root == nullptr)
+        if (root == NULL) //  first and second are used to rod and skip houses.
             return {0, 0};
 
         auto leftans = solve(root->left);
@@ -24,16 +24,16 @@ public:
                        + rightans.second;
 
         // If we don't rob this node, choose the best option for each child
-        int skipCurrent = max(leftans.first, leftans.second)
-                        + max(rightans.first, rightans.second);
+        int skipCurrent = max(leftans.first, leftans.second) // Pick whichever is better: rob left or skip left.
+                        + max(rightans.first, rightans.second); // Pick whichever is better: rob right or skip right.
 
-        return {robCurrent, skipCurrent};
+        return {robCurrent, skipCurrent}; // return possibility
     }
 
     int rob(TreeNode* root) {
-        auto ans = solve(root);
+        auto ans = solve(root); // Calculate the two possibilities for the entire tree.
 
-        return max(ans.first, ans.second);
+        return max(ans.first, ans.second); // choose which ever gives more money.
     }
 };
 
