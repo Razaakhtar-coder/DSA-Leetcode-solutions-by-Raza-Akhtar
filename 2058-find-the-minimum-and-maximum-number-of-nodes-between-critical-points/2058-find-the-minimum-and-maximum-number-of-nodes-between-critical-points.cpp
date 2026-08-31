7c62738274fd0563ad9a14dc7188ea5ee8e9802b
Razaakhtar-coder <akhtarrakeeb52@gmail.com>
2026-08-31 17:05:59 +0530

    Time: 0 ms (100%), Space: 124.5 MB (95.97%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        int minDistance = INT_MAX;
+
+        ListNode* prev = head;
+        ListNode* curr = head->next;
+        int currPosition = 1;
+        int previousCriticalIndex = 0;
+        int firstCriticalIndex = 0;
+
+        while (curr->next != NULL) {
+            
+            // When we see a critical point
+            if ((curr->val < prev->val &&
+                 curr->val < curr->next->val) ||
+                (curr->val > prev->val &&
+                 curr->val > curr->next->val)) {
+
+                    if (previousCriticalIndex == 0) {
+                        previousCriticalIndex = currPosition;
+                        firstCriticalIndex = currPosition;
+                    } else {
+                        minDistance =
+                            min(minDistance, currPosition - previousCriticalIndex);
+                        previousCriticalIndex = currPosition;
+                    }
+            }
+
+            currPosition++;
+            prev = curr;
+            curr = curr->next;
+        }
+
+        if (minDistance == INT_MAX) {
+            return {-1, -1};
+        }
+
+        int maxDistance = previousCriticalIndex - firstCriticalIndex; // found the min distance now use formula (previousCriticalIndex - firstCriticalIndex) to get maxDist ex- prevcritical = 5 and firstcritical = 2 than, 5-2 = 3 is the maxDist. 
+        return {minDistance, maxDistance};
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
