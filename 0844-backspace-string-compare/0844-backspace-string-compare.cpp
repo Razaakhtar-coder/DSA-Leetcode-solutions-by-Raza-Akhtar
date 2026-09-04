c4084afe00d36e229ba9e8a58b45a78d29901349
Razaakhtar-coder <akhtarrakeeb52@gmail.com>
2026-09-04 14:46:48 +0530

    Time: 0 ms (100%), Space: 8.5 MB (68.87%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -5,26 +5,82 @@ public:  //Time: O(n + m)
         string a = "", b = "";
 
         for (char c : s) {
-            if (c == '#') {
-                if (!a.empty())
+            if (c == '#') { // if char = #
+                if (!a.empty()) // and if 'a' string is not empty
                     a.pop_back();
             } else {
-                a += c;
+                a += c; // add char to 'a'
             }
         }
 
-        for (char c : t) {
-            if (c == '#') {
-                if (!b.empty())
+        for (char c : t) { 
+            if (c == '#') { // if char = #
+                if (!b.empty()) // and if 'b' string is not empty
                     b.pop_back();
             } else {
-                b += c;
+                b += c; // add char to 'b'
             }
         }
 
         return a == b;
     }
 };
+
+// class Solution { // t.c -> 0(n + m), s.c -> 0(1)
+// public:
+//     bool backspaceCompare(string s, string t) {
+//         int i = s.size() - 1;
+//         int j = t.size() - 1;
+
+//         int skipS = 0, skipT = 0;
+
+//         while (i >= 0 || j >= 0) {
+
+//             // Find next valid character in s
+//             while (i >= 0) {
+//                 if (s[i] == '#') {
+//                     skipS++;
+//                     i--;
+//                 } 
+//                 else if (skipS > 0) {
+//                     skipS--;
+//                     i--;
+//                 } 
+//                 else {
+//                     break;
+//                 }
+//             }
+
+//             // Find next valid character in t
+//             while (j >= 0) {
+//                 if (t[j] == '#') {
+//                     skipT++;
+//                     j--;
+//                 } 
+//                 else if (skipT > 0) {
+//                     skipT--;
+//                     j--;
+//                 } 
+//                 else {
+//                     break;
+//                 }
+//             }
+
+//             // Compare valid characters
+//             if (i >= 0 && j >= 0 && s[i] != t[j])
+//                 return false;
+
+//             // One string has a character, the other doesn't
+//             if ((i >= 0) != (j >= 0))
+//                 return false;
+
+//             i--;
+//             j--;
+//         }
+
+//         return true;
+//     }
+// };
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
