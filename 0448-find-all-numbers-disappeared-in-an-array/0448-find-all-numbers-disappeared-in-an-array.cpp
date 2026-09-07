068d1e3ec6b82643c7aa8063f5e40f5bc5e20981
Razaakhtar-coder <akhtarrakeeb52@gmail.com>
2026-09-07 20:59:32 +0530

    Enhance code clarity with comments
    Added comments to clarify the logic for marking existing numbers and identifying missing numbers.

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -5,16 +5,16 @@ public:
 
         // Mark numbers that exist
         for (int num : nums) {
-            int idx = abs(num) - 1;
-            nums[idx] = -abs(nums[idx]);
+            int idx = abs(num) - 1; // substract 1 from the number's idx to get the original number
+            nums[idx] = -abs(nums[idx]); // convert it to negative after going to the number
         }
 
         // Positive positions represent missing numbers
         vector<int> ans;
 
         for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) {
-                ans.push_back(i + 1);
+            if (nums[i] > 0) { // check if we have already taken the number
+                ans.push_back(i + 1); // push ans
             }
         }
 
