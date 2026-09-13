41478d3a3d52bdfcf7574ff040c959761c038dcd
Razaakhtar-coder <akhtarrakeeb52@gmail.com>
2026-09-13 14:56:19 +0530

    Time: 0 ms (100%), Space: 9.9 MB (89.51%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isFreqSame(int freq1[], int freq2[]) {
+        for(int i = 0; i < 26; i++) {
+            if(freq1[i] != freq2[i])
+                return false;
+        }
+        return true;
+    } 
+
+    bool checkInclusion(string s1, string s2) {
+        if(s1.length() > s2.length())
+            return false;
+
+        int freq1[26] = {0};
+        int freq2[26] = {0};
+
+        for(int i = 0; i < s1.length(); i++) {
+            freq1[s1[i] - 'a']++;
+            freq2[s2[i] - 'a']++;
+        }
+
+        if(isFreqSame(freq1, freq2))
+            return true;
+
+        for(int i = s1.length(); i < s2.length(); i++) {
+            freq2[s2[i] - 'a']++;  // add new char
+            freq2[s2[i - s1.length()] - 'a']--;  // remove old char
+
+            if(isFreqSame(freq1, freq2))
+                return true;
+        }
+
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
