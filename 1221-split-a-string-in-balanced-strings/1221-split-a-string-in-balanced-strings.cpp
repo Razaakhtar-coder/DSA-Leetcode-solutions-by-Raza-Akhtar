27a157b3707101d180307d1b73b290af2833b677
Razaakhtar-coder <akhtarrakeeb52@gmail.com>
2026-09-24 18:42:50 +0530

    Time: 0 ms (100%), Space: 8.4 MB (17.8%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int n = s.size();
+
+        int count = 0;
+        int ans = 0;
+        
+        for(int i=0;i<n; i++){
+            if(s[i] == 'R'){ // substr should start with R, given i question
+            count++; // increase count
+
+            } else{
+                count--; // or decrease count if letters are not equal in quantity
+            }
+
+            if(count == 0){
+                ans++;
+            }
+        }
+        return ans;
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
